a052c3b1b98857617639b5a8eadedd8f62b277f0
pungki14117154 <50086343+pungki14117154@users.noreply.github.com>
2019-10-25 18:09:13 +0700

    Ubah File latihan 1

--- lat_1.php
+++ lat_1.php
@@ -5,17 +5,17 @@ function swap(&$a,&$b){
 	$b=$c;;
 	
 	return 0;
-	
 }
+
 $c=0;
 $a=1;
 $b=3;
-echo "Nilai sebelum ditukar </br>";
+echo "Nilai sebelum diswap </br>";
 echo "A = 1 </br>"; 
 echo "B = 3 </br>";
 
 swap($a,$b);
-echo "Nilai Sesudah ditukar </br>";
+echo "Nilai Sesudah diswap </br>";
 echo "A = $a </br>";
 echo "B = $b </br>";
 ?>
