dc3f4e0edebde75afee7994802a5bef324cbf4ca
pungki14117154 <50086343+pungki14117154@users.noreply.github.com>
2019-11-07 12:05:13 +0700

    Hapus file yang bermasalah

--- minggu_6/latihan1_operator.php
+++
@@ -1,9 +0,0 @@
-<?php
-$a = 3;
-$b = 7;
-$a += 5; //$a= $a +5;
-$b = ($c = 11) + 3;
-echo "nilai Variabel a adalah = $a<br />";
-echo "nilai Variabel b adalah = $b<br />";
-echo "nilai Variabel c adalah = $c<br />";
-?>
--- minggu_6/latihan3.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$aray =["lanirne","aduh","qifuat","toda","anevi","samid","kifuat"];
-sort($aray);
-
-print_r ($aray);
-echo "<br>";
-?>
